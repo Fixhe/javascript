5f1a17b3e833f5ad50ba663e45e40c66093ac271
Fixhe <ravinmengheang830@gmail.com>
2024-03-01 14:24:51 +0700

    CounterProgram

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// const=variable that can't change
+// Counter Program
 
-const PI=3.14
-let radius;
-let circumference;
-radius = Number(radius)
-circumference=radius*2
-document.getElementById("mySubmit").onclick=function(){
-    radius=document.getElementById("mytext").value 
-    radius=Number(radius)
-    circumference=2*PI*radius
-    document.getElementById("myH3").textContent=circumference
+ const decreasebt=document.getElementById("decreasebt");
+ const resetbt=document.getElementById("resetbt")
+ const increasebt=document.getElementById("increasebt")
+ const countLabel=document.getElementById("countLabel");
+ let count=0;
+increasebt.onclick=function(){
+    count++;
+    countLabel.textContent=count;
+}
+decreasebt.onclick=function(){
+    count--;
+    countLabel.textContent=count;
+}
+resetbt.onclick=function(){
+    count=0;
+    countLabel.textContent=count;
 }
