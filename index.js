ad6afb4ce4429dc27c8a44a4267fb41860d196e9
Fixhe <ravinmengheang830@gmail.com>
2024-03-22 16:16:38 +0700

    ifcondition

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-//RAMDOMNUMBER GENERATOR
-const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
-const max=100;
-const min=50;
-let randomNum;
-myButton.onclick = function(){
-    randomNum = Math.floor(Math.random() *max)+min;
-    myLabel.textContent = randomNum;
+//if statement=if a condition is true, execute some code 
+//              if not, do something else
+
+const myInput=document.getElementById("myInput");
+const mySubmit=document.getElementById("mySubmit");
+const result=document.getElementById("result");
+
+let age;
+mySubmit.onclick=function(){
+    age=myInput.value;
+    if(age>100){
+        result.textContent=(`you are oldder`)
+    }if (age>=18) {
+        result.textContent=(`you can enter this site`)
+    }if(age<18){
+        result.textContent=(`you can't enter this site`)
+    }if(age==0){
+        result.textContent=(`you have had born yet`)
+    }
+
 }
