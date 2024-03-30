7e69201936b451d5780484bac4ad28c84094309c
Fixhe <ravinmengheang830@gmail.com>
2024-03-30 19:07:52 +0700

    ternaryOperator

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-//check = property that determines the checked state of an 
-//        HTML checkbox or radio button element
+// ternary operator = a shortcut to if{} and else{} statement
+//               help to assign variable based on a condition
+//              condition? codeiftrue : codeiffale;
 
-const myCheckbox = document.getElementById("myCheckbox");
-const myVisaBtn = document.getElementById("myVisaBtn");
-const myMastercardBtn = document.getElementById("myMastercardBtn");
-const myPaypalBtn = document.getElementById("myPaypalBtn");
-const mySubmit = document.getElementById("mySubmit");
-const resultSubmit = document.getElementById("resultSubmit");
-const resultPayment = document.getElementById("resultPayment");
+// let age=17;
+// let message = age>18?`your are adult`:`you are a minor`;
+// console.log(message)
 
-mySubmit.onclick = function(){
-    if(myCheckbox.checked){
-        resultSubmit.textContent = `you are subscribed`;
+// let purchamoung=200;
+// let discound = purchamoung>=100? 50 : 0;
+// console.log(`Payment is $${purchamoung-purchamoung*(discound/100)}`)
+
+const myInput=document.getElementById("myInput");
+const mySubmit=document.getElementById("mySubmit");
+const result=document.getElementById("result");
+let discount;
+mySubmit.onclick=function(){
+    const inputValue=parseFloat(myInput.value);
+    if(!isNaN(inputValue)){
+    discount=inputValue>=100? 50 : 0;
+    const payment=inputValue-inputValue*(discount/100);
+    result.textContent=`Payment is $${payment.toFixed(2)}`;
     }else{
-        resultSubmit.textContent = `you are not subscribed`;
+        result.textContent=`Please input value `
     }
-    if(myVisaBtn.checked){
-        resultPayment.textContent = `you are paying with Visa`;
-    }else if(myMastercardBtn.checked){
-        resultPayment.textContent=`you are paying with mastercard`;
-    }else if(myPaypalBtn.checked){
-        resultPayment.textContent=`you are paying with Paypal`;
-    }else{
-        resultPayment.textContent=`you must select one all of payment`;
-    }
+
 }
