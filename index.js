9c70ce263b37a5942713b8996a693d6e08dc84a9
Fixhe <ravinmengheang830@gmail.com>
2024-04-22 13:49:29 +0700

    switch

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-// ternary operator = a shortcut to if{} and else{} statement
-//               help to assign variable based on a condition
-//              condition? codeiftrue : codeiffale;
+//Switch = can be an efficient replacement to many else if 
+//         statements;
 
-// let age=17;
-// let message = age>18?`your are adult`:`you are a minor`;
-// console.log(message)
-
-// let purchamoung=200;
-// let discound = purchamoung>=100? 50 : 0;
-// console.log(`Payment is $${purchamoung-purchamoung*(discound/100)}`)
-
-const myInput=document.getElementById("myInput");
-const mySubmit=document.getElementById("mySubmit");
-const result=document.getElementById("result");
-let discount;
-mySubmit.onclick=function(){
-    const inputValue=parseFloat(myInput.value);
-    if(!isNaN(inputValue)){
-    discount=inputValue>=100? 50 : 0;
-    const payment=inputValue-inputValue*(discount/100);
-    result.textContent=`Payment is $${payment.toFixed(2)}`;
-    }else{
-        result.textContent=`Please input value `
-    }
-
+let testScore=40;
+let testGrade;
+switch(true){
+    case testScore>=50:
+        testGrade="pass";
+         break;
+    case testScore<50:
+        testGrade="fail";
+        break;
+    default:
+        testGrade="Absent";
 }
+console.log(`Result is ${testGrade}`);
